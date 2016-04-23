3a6e59e7cd5bd4543a075d12c8e9b881f4bc214f
Mikhail Pimenov <mupimenov@gmail.com>
2016-04-23 13:54:38 +0300

    [+] забытый заголовочный файл

---
+++ src/hponic/arduino.h
@@ -0,0 +1,16 @@
+#ifndef ARDUINO_H_
+#define ARDUINO_H_
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+unsigned long millis(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ARDUINO_H_ */
